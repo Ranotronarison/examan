969de75bb1630dc778702fc48cb46cf4dfb5fc22
Ranotronarison <arivonyran@gmail.com>
2025-09-13 18:26:10 +0300

    chore: Update start script to streamline submodule setup and enhance initialization process

--- start.sh
+++ start.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-if [ ! -d "./examan-api" ] || [ -z "$(ls -A ./examan-api)" ]; then
-  echo "Cloning examan-api repository..."
-  git clone git@github.com:Ranotronarison/examan-api.git && git submodule update --init --recursive
-else
-  echo "examan-api repository already exists. Skipping clone."
-fi
+# Array of submodule configurations: "folder_name:repository_url"
+SUBMODULES=(
+  "examan-api:git@github.com:Ranotronarison/examan-api.git"
+  "examan-front:git@github.com:Ranotronarison/examan-front.git"
+)
 
-# make sure all submodules are initialized and updated
-git submodule update --remote examan-api
+# Function to clone or update submodules
+setup_submodules() {
+  for submodule in "${SUBMODULES[@]}"; do
+    IFS=':' read -r folder_name repo_url <<< "$submodule"
+    
+    if [ ! -d "./$folder_name" ] || [ -z "$(ls -A ./$folder_name)" ]; then
+      echo "Cloning $folder_name repository..."
+      git clone "$repo_url" && git submodule update --init --recursive
+    else
+      echo "$folder_name repository already exists. Skipping clone."
+    fi
+  done
+}
+
+# Setup all submodules
+setup_submodules
+
+# Make sure all submodules are initialized and updated
+echo "Updating all submodules..."
+for submodule in "${SUBMODULES[@]}"; do
+  IFS=':' read -r folder_name repo_url <<< "$submodule"
+  git submodule update --remote "$folder_name"
+done
 
 # run docker compose
-docker compose up -d --build --wait
+docker compose up -d --wait
 
 # run composer install in examan-api
 docker compose exec -it examan-api composer install
